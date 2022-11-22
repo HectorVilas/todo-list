dcf322a7c1b2cd67d0d5d9f85bb410faf74fa82a
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-11-22 15:03:03 -0300

    general changes for testing

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log("test")
+import "./style.css";
+import {test} from "./test.js";
+
+test()
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,27 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: "development",
-  entry: './src/index.js',
+  // mode: "production",
+  entry: {index:'./src/index.js', test:'./src/test.js',},
+  devtool: "inline-source-map",
+  // devServer: { static: './dist' },
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules:[
+      {test: /\.css$/i, use:['style-loader', 'css-loader']}
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Odin TODO List',
+    }),
+  ],
+  // optimization: {
+  //   runtimeChunk: 'single',
+  // },
 };
