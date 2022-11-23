544c868083128fcbcb9a12c87bb617e0d8bf5c94
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-11-23 19:27:35 -0300

    add main and nav items, testing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,29 @@
 import "./style.css";
-import {test} from "./test.js";
 
-test()
+const ui = (() => {
+  const body = document.querySelector("body");
+  
+  function loadInterface(){
+    const main = document.createElement("div");
+    main.id = "main";
+
+    const nav = document.createElement("div");
+    nav.id = "nav";
+
+    const btnsText = ["search", "home", "add"];
+    for (let i = 0; i < btnsText.length; i++) {
+      const div = document.createElement("div");
+      div.classList.add("nav-btn");
+      div.innerText = btnsText[i];
+
+      nav.appendChild(div);
+    };
+
+    body.appendChild(main);
+    body.appendChild(nav);
+  };
+
+  return { loadInterface };
+})();
+
+ui.loadInterface();
