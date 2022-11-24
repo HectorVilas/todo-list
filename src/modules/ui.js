cae2157fa691f603d54c9cae4e99486c05570630
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-11-24 19:13:52 -0300

    move UI related function to it's own module

---
+++ src/modules/ui.js
@@ -0,0 +1,25 @@
+export const ui = (() => {
+  const body = document.querySelector("body");
+  
+  function loadInterface(){
+    const main = document.createElement("div");
+    main.id = "main";
+
+    const nav = document.createElement("div");
+    nav.id = "nav";
+
+    const btnsText = ["search", "home", "add"];
+    for (let i = 0; i < btnsText.length; i++) {
+      const div = document.createElement("div");
+      div.classList.add("nav-btn");
+      div.innerText = btnsText[i];
+
+      nav.appendChild(div);
+    };
+
+    body.appendChild(main);
+    body.appendChild(nav);
+  };
+
+  return { loadInterface };
+})();
