0a8babe89727eef608337d449c23b4513190e4f6
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-11-25 19:03:38 -0300

    add image module

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,8 @@ module.exports = {
   },
   module: {
     rules:[
-      {test: /\.css$/i, use:['style-loader', 'css-loader']}
+      {test: /\.css$/i, use:['style-loader', 'css-loader']},
+      {test: /\.(png|svg|jpg|jpeg|gif)$/i, type: 'asset/resource'},
     ],
   },
   plugins: [
