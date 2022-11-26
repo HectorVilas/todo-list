6fade66981044b31e744899785c79c024dca166f
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-11-26 17:39:55 -0300

    add function to place todo card

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,3 +1,10 @@
+import iconCheckmark from "../media/images/icons/checkmark.svg";
+import iconDrag from "../media/images/icons/drag.svg";
+import iconEdit from "../media/images/icons/edit.svg";
+import iconTrash from "../media/images/icons/trash.svg";
+import iconTrashLid from "../media/images/icons/trash-lid.svg";
+
+
 export const ui = (() => {
   const body = document.querySelector("body");
   
@@ -10,5 +17,47 @@ export const ui = (() => {
     body.appendChild(main);
   };
 
-  return { loadInterface };
+  function placeCard(todo){
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    const drag = document.createElement("img");
+    drag.classList.add("card-icon", "icon-drag");
+    drag.src = iconDrag;
+
+    const title = document.createElement("h1");
+    title.classList.add("title");
+    title.innerText = todo.title;
+
+    const description = document.createElement("p");
+    description.classList.add("description");
+    description.innerText = todo.description;
+
+    const checks = document.createElement("ul");
+    checks.classList.add("checklist");
+
+    todo.checks.forEach((check,i) => {
+      const task = document.createElement("li");
+
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.id = `${todo.title}-${i}`;
+      checkbox.checked = check.isDone;
+
+      const label = document.createElement("label");
+      label.htmlFor = checkbox.id;
+      label.innerText = check.task;
+
+      task.append(checkbox, label);
+
+      checks.appendChild(task);
+
+    });
+
+    card.append(drag, title, description, checks);
+
+    return card;
+  }
+
+  return { loadInterface, placeCard };
 })();
