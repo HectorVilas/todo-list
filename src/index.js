ac811e60f4b215b5d4508b809b3efc5c17c17389
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-11-27 19:15:28 -0300

    add more objects for testing

--- src/index.js
+++ src/index.js
@@ -29,11 +29,32 @@ const projects = [
 
       new Todo("test2", "test2 descr", "2022-11-23T17:42", "2023-11-23T17:42", [
         { task: "test2 task1", isDone: true },
-        { task: "test2 task2", isDone: true },
-        { task: "test3 task3", isDone: true },
-        { task: "test4 task4", isDone: true },
+        { task: "test2 task2", isDone: false },
+        { task: "test2 task3", isDone: true },
+        { task: "test2 task4", isDone: false },
+        { task: "test2 task5", isDone: false },
+      ], "middle", true),
+      new Todo("test3", "test3 descr", "2022-11-23T17:42", "2023-11-23T17:42", [
+        { task: "test3 task1", isDone: true },
+        { task: "test3 task2", isDone: true },
+        { task: "test3 task3", isDone: false },
+        { task: "test3 task4", isDone: false },
+        { task: "test3 task5", isDone: true },
+      ], "low", false),
+      new Todo("test4", "test4 descr", "2022-11-23T17:42", "2023-11-23T17:42", [
+        { task: "test4 task1", isDone: false },
+        { task: "test4 task2", isDone: false },
+        { task: "test4 task3", isDone: true },
+        { task: "test4 task4", isDone: false },
+        { task: "test4 task5", isDone: true },
+      ], "low", true),
+      new Todo("test5", "test5 descr", "2022-11-23T17:42", "2023-11-23T17:42", [
+        { task: "test5 task1", isDone: false },
+        { task: "test5 task2", isDone: true },
+        { task: "test5 task3", isDone: true },
+        { task: "test5 task4", isDone: false },
         { task: "test5 task5", isDone: true },
-      ], "middle", false),
+      ], "high", false),
     ],
   },
 ];
