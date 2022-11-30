4eb2c8252d748b998a890300d8bdcba4b0d9a8d7
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-11-30 14:17:28 -0300

    move test function from ui.js

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -6,6 +6,67 @@ import iconTrashLid from "../media/images/icons/trash-lid.svg";
 import iconFav from "../media/images/icons/fav.svg";
 import iconFavActive from "../media/images/icons/fav-active.svg";
 
+//for testing
+import { Todo } from "../modules/classes.js";
+export const localStorageTest = [
+  {
+    title: "routine",
+    todos: [
+      new Todo("exercise", "It's leg day!", "2022-11-23T17:42", null, [
+        { task: "run 3km", isDone: true },
+        { task: "bicycle", isDone: false },
+        { task: "squats", isDone: false },
+      ], "low", false, false),
+
+      new Todo("coding", null, "2022-11-23T17:42", null, [
+        { task: "check css-tricks", isDone: true },
+        { task: "continue TOP practice", isDone: true },
+      ], "high", true, true),
+    ],
+  },
+  {
+    title: "test",
+    todos: [
+      new Todo("test1", "test1 descr", "2022-11-23T17:42","2023-11-23T17:42", [
+        { task: "test1 task1", isDone: true },
+        { task: "test1 task2", isDone: false },
+        { task: "test1 task3", isDone: true },
+      ], "low", true, false),
+
+      new Todo("test2", "test2 descr", "2022-11-23T17:42", "2023-11-23T17:42", [
+        { task: "test2 task1", isDone: true },
+        { task: "test2 task2", isDone: false },
+        { task: "test2 task3", isDone: true },
+        { task: "test2 task4", isDone: false },
+        { task: "test2 task5", isDone: false },
+      ], "middle", true, false),
+      new Todo("test3", "test3 descr", "2022-11-23T17:42", "2023-11-23T17:42", [
+        { task: "test3 task1", isDone: true },
+        { task: "test3 task2", isDone: true },
+        { task: "test3 task3", isDone: false },
+        { task: "test3 task4", isDone: false },
+        { task: "test3 task5", isDone: true },
+      ], "low", false, true),
+      new Todo("test4", "test4 descr", "2022-11-23T17:42", "2023-11-23T17:42", [
+        { task: "test4 task1", isDone: false },
+        { task: "test4 task2", isDone: false },
+        { task: "test4 task3", isDone: true },
+        { task: "test4 task4", isDone: false },
+        { task: "test4 task5", isDone: true },
+      ], "low", true, false),
+      new Todo("test5", "test5 descr", "2022-11-23T17:42", "2023-11-23T17:42", [
+        { task: "test5 task1", isDone: false },
+        { task: "test5 task2", isDone: true },
+        { task: "test5 task3", isDone: true },
+        { task: "test5 task4", isDone: false },
+        { task: "test5 task5", isDone: true },
+      ], "high", false, false),
+    ],
+  },
+];
+
+
+
 
 export const ui = (() => {
   const body = document.querySelector("body");
