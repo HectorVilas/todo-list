4d29360be02b66e98da2f131d073d41655484d43
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-12-15 18:08:52 -0300

    remove test code lines

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 import "./style.css";
 import { ui } from "./modules/ui.js";
 
-//for testing
-import { localStorageTest } from "./modules/localStorageTest.js";
-
-window.addEventListener("keydown", (e) => {
-  if(e.key === "r" && e.altKey){
-    ui.placeCards(localStorageTest);
-    console.log("replacing main");
-  };
-});
-
-
 //run on load
 ui.loadMenu();
-ui.placeCards(localStorageTest);
+ui.placeCards(0);
