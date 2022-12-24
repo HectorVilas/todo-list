f66c2adfd72448793592f2def9085ab4edaba8f0
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-12-24 19:03:37 -0300

    remove testing code

--- src/index.js
+++ src/index.js
@@ -4,9 +4,3 @@ import { ui } from "./modules/ui.js";
 //run on load
 ui.loadMenu();
 ui.placeCards(0);
-
-
-
-//for testing
-import { todoSample } from "./modules/todoSample";
-localStorage.setItem("todo", JSON.stringify(todoSample));
